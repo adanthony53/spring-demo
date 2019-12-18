0c2b42e1ad8b3ac0fac0a4515f9b113f94b093f4
adanthony53 <dmz050@uw.edu>
2019-12-18 14:54:42 -0800

    added github login

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## spring demo exercise
 
 spring demo exercise
+
+GitHub OAuth LogIn Info
+https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/
+
+OkHttp
+https://square.github.io/okhttp/
---
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.AccessTokenDTO;
+import com.example.demo.dto.GithubUser;
+import com.example.demo.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("6eab45a163e8b4b9b522");
+        accessTokenDTO.setClient_secret("2b09e36450b60198b6e1a61f6d1344322c9ea36b");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_url("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        //System.out.println(user.getName());
+        return "index";
+    }
+}
