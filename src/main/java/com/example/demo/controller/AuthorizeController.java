9149ec9e9e74c0b04452daf2221767b0221447d3
adanthony53 <dmz050@uw.edu>
2019-12-18 21:16:57 -0800

    add publish template

--- src/main/java/com/example/demo/controller/AuthorizeController.java
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -32,8 +32,8 @@ public class AuthorizeController {
     private String redirectUrl;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state,
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
                            HttpServletRequest request,
                            HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
